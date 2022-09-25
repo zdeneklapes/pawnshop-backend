7e9528264e415170af833de18e59b6b4a4ebea4e
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-09-25 15:23:56 +0200

    .

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -8,9 +8,9 @@ if [ "$1" == "build" ]; then
     python manage.py migrate --no-input
 #    gunicorn core.wsgi:application -b 0.0.0.0:"$PORT"
 elif [ "$1" == "dev" ]; then
-    #    python src/manage.py wait_for_db
-    #    python src/manage.py makemigrations --no-input
-    #    python src/manage.py migrate --no-input
+    #    python3 src/manage.py wait_for_db
+    python3 manage.py migrate --no-input
+    python3 manage.py makemigrations --no-input
     gunicorn --timeout 1000 --workers=3 --bind=0.0.0.0:$PORT --log-level debug src.wsgi:application
 #    poetry run python manage.py runserver 0.0.0.0:"$PORT"
 elif [ "$1" == "dev_local" ]; then
