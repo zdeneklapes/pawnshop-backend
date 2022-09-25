03aade23d269684141d8ff7702c672b761c8d665
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-09-25 14:55:04 +0200

    .

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -2,15 +2,10 @@
 
 if [ "$1" == "build" ]; then
     cd backend || exit 1
-
     python manage.py wait_for_db
-
     python manage.py collectstatic --no-input
-
     python manage.py makemigrations --no-input
-
     python manage.py migrate --no-input
-
 #    gunicorn core.wsgi:application -b 0.0.0.0:"$PORT"
 elif [ "$1" == "dev" ]; then
     #    python src/manage.py wait_for_db
@@ -20,14 +15,10 @@ elif [ "$1" == "dev" ]; then
 #    poetry run python manage.py runserver 0.0.0.0:"$PORT"
 elif [ "$1" == "dev_local" ]; then
     python manage.py wait_for_db
-
     python manage.py makemigrations --no-input
-
     python manage.py migrate --no-input
-
     python manage.py runserver
 elif [ "$1" == "prod" ]; then
     python3 backend/manage.py makemigrations --no-input
-
     python3 backend/manage.py migrate --no-input
 fi
