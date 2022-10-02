560bac7974f98a2bab1b9f68386af6bda0e83adf
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-02 16:54:01 +0200

    .

--- src/product/views/filters.py
+++ src/product/views/filters.py
@@ -1,19 +1,10 @@
 from rest_framework import filters
 import coreapi
 
+
 class ProductFilters(filters.BaseFilterBackend):
     def get_schema_fields(self, view):
         return [
-            coreapi.Field(
-                name="status",
-                location='query',
-                required=False,
-                type='string'
-            ),
-            coreapi.Field(
-                name="operation",
-                location='query',
-                required=False,
-                type='string'
-            ),
+            coreapi.Field(name="status", location="query", required=False, type="string"),
+            coreapi.Field(name="operation", location="query", required=False, type="string"),
         ]
