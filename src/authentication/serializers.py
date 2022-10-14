9c3e9126158347bd910265221bb785056cffc93a
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-14 20:47:24 +0200

    .

--- src/authentication/serializers.py
+++ src/authentication/serializers.py
@@ -58,6 +58,7 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
+        token["email"] = user.email
         token["role"] = user.role
         return token
 
