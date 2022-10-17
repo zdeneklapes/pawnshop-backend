73c5effbc3d0132e90ed16b05f9ed79cca305b73
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-17 18:08:10 +0200

    .

--- src/authentication/urls.py
+++ src/authentication/urls.py
@@ -6,8 +6,8 @@ from . import views
 
 # Automatically find all url based on ViewSet (all?)
 router = routers.DefaultRouter()
-router.register(prefix=r"users", viewset=views.UserViewSet)
-router.register(prefix=r"attendants", viewset=views.AttendantProfileCreateViewSet)
+router.register(prefix=r"user", viewset=views.UserViewSet)
+router.register(prefix=r"attendant", viewset=views.AttendantProfileCreateViewSet)
 
 urlpatterns = [
     # Authentication
--- src/authentication/views.py
+++ src/authentication/views.py
@@ -9,7 +9,7 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser] if AUTH else [permissions.AllowAny]
-    http_method_names = ["get", "patch"]
+    http_method_names = ["get", "post", "patch"]
 
 
 class AttendantProfileCreateViewSet(viewsets.ModelViewSet):
