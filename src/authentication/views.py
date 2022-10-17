42ac0dbcaa73d4e27ecf4def927941ea729d7511
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-17 17:54:30 +0200

    .

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,14 +1,15 @@
-from rest_framework import mixins, viewsets, permissions
+from rest_framework import permissions, viewsets
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from . import models, serializers
 from config.settings import AUTH
 
 
-class UserViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializer
     permission_classes = [permissions.IsAuthenticated, permissions.IsAdminUser] if AUTH else [permissions.AllowAny]
+    http_method_names = ["get"]
 
 
 class AttendantProfileCreateViewSet(viewsets.ModelViewSet):
