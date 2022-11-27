c58d776416cd6760fd181b37163d1170319f528d
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-11-27 19:54:17 +0100

    .

--- src/product/serializers/product_shop_state.py
+++ src/product/serializers/product_shop_state.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
 
+from product.models import Product
 
-class ProductShopStateSerializer(serializers.Serializer):
+
+class ProductShopStateSerializer(serializers.ModelSerializer):
     status = serializers.CharField()
+    # status = serializers.ChoiceField(
+    #     source="get_description_display", choices=ProductStatusOrData, read_only=True
+    # )
     count = serializers.IntegerField()
     buy = serializers.IntegerField()
     sell = serializers.IntegerField()
+
+    class Meta:
+        model = Product
+        fields = ["status", "count", "buy", "sell"]
---
+++ src/product/tests/test_shop_stats.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.mark.django_db
+@pytest.mark.skip(reason="TODO test")
+def test_shop_stat_expected_data(client_admin, load_all_scope_module):
+    pass
