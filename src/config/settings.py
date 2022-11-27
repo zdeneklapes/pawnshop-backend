0f010cb3ee2cf9bd7706968e810dfd6432d2b109
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-11-27 03:15:33 +0100

    test

--- src/config/settings.py
+++ src/config/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
     "django_filters",
-    "wkhtmltopdf",
+    # "wkhtmltopdf",
     "debug_permissions",
     #
     "authentication",
