1ebb7148aae83b4dbe6b931379600fab374eb463
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-11-28 18:06:13 +0100

    fizex cz grmatics

--- src/statistic/models/choices.py
+++ src/statistic/models/choices.py
@@ -10,19 +10,19 @@ class StatisticQueryParams(models.TextChoices):
 
 class StatisticDescription(models.TextChoices):
     # Loans
-    LOAN_CREATE = "LOAN_CREATE", "Zastava"
-    LOAN_EXTEND = "LOAN_EXTEND", "Prodlouzeni"
-    LOAN_EXTEND_AFTER_MATURITY = "LOAN_EXTEND_AFTER_MATURITY", "Prodlouzeni po splatnosti"
-    LOAN_RETURN = "LOAN_RETURN", "Vyber"
+    LOAN_CREATE = "LOAN_CREATE", "Zástava"
+    LOAN_EXTEND = "LOAN_EXTEND", "Prodloužení"
+    LOAN_EXTEND_AFTER_MATURITY = "LOAN_EXTEND_AFTER_MATURITY", "Prodloužení po splatnosti"
+    LOAN_RETURN = "LOAN_RETURN", "Výběr"
     # Offers
-    OFFER_BUY = "OFFER_BUY", "Vykup"
+    OFFER_BUY = "OFFER_BUY", "Výkup"
     OFFER_SELL = "OFFER_SELL", "Prodej"
     # Move
-    LOAN_TO_OFFER = "LOAN_TO_OFFER", "Presunuti do bazaru"
+    LOAN_TO_OFFER = "LOAN_TO_OFFER", "Přesunutí do bazaru"
     #
-    UPDATE_DATA = "UPDATE_DATA", "Produkt aktualizovan"
+    UPDATE_DATA = "UPDATE_DATA", "Produkt aktualizován"
     # Logins
-    LOGIN = "LOGIN", "Prihlaseni"
-    LOGOUT = "LOGOUT", "Odhlaseni"
+    LOGIN = "LOGIN", "Přihlášení"
+    LOGOUT = "LOGOUT", "Odhlášení"
     #
-    RESET = "RESET", "Reset profit"
+    RESET = "RESET", "Profit nulován"
